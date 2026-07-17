2508c2fb487748e06b2120d1eb306e43b9a205e2
Caleb Wietholter <calebwietholter9@outlook.com>
2026-07-17 13:58:32 -0400

    prototyped api.ts for frontend

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,3 +1,4 @@
+// todo: test this shit
 export async function createUser(username: string, password: string) {
     const response = await fetch("/createuser", {
         method: "POST",
@@ -13,6 +14,50 @@ export async function createUser(username: string, password: string) {
     return apikey;
 }
 
-// todo: create login function
+// todo: test this shit
+export async function login(username: string, password: string) {
+    const response = await fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+    });
+    const apikey = await response.text();
+    return apikey;
+}
+
+//todo: test this shit
+export async function getData(apikey: string) {
+    const response = await fetch("/data", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "apikey": apikey
+        })
+    });
+    const data = await response.json();
+    return data;
+}
+
+// todo: test this shit
+export async function setData(apikey: string, data: JSON) {
+    const response = await fetch("/data", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "apikey": apikey,
+            "data": data
+        })
+    });
+    return response.ok;
+}
 // todo: create setData function
 // todo: create getData function
