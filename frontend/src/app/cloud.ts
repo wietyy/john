87ae09cc409417afab3f9874daf24bb43a711211
Caleb Wietholter <44294279+wietyy@users.noreply.github.com>
2026-09-24 13:09:58 -0400

    Added cloud.ts, cloud operations now visible client-side. We might be almost done

--- frontend/src/app/cloud.ts
+++ frontend/src/app/cloud.ts
@@ -1,7 +1,23 @@
-export async function getCloud() {
-    
+export async function getCloud(password: string): Promise<string> {
+    const response = await fetch("/api/getCloudData", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ password }),
+    });
+    const data = await response.json();
+    return data.data ?? "";
 }
 
-export async function setCloud() {
-    
+export async function setCloud(password: string, data: string): Promise<string> {
+    const response = await fetch("/api/setCloudData", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ password, data }),
+    });
+    const result = await response.json();
+    return result.result ?? "error";
 }
