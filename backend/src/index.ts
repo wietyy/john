267a5d800b0e859fa20b2ff2099617e6d54d4db2
Caleb Wietholter <44294279+wietyy@users.noreply.github.com>
2026-09-24 17:44:52 -0400

    Added rate limiting to express to prevent attacks

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import { getData, setData } from './db';
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes in milliseconds
+    limit: 100,
+    message: "Too many requests",
+});
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static('frontend/dist'));
+app.use(limiter);
 
 app.post('/api/getCloudData', (req, res) => {
     const password = req.body.password;
